83e95fe1694241458f9a4cfd522fee9f00bcbdb5
MarRaV6 <maryaraspopova@gmail.com>
2016-05-16 17:11:14 +0300

    добавлены формы и скрипт для меню

--- main.py
+++ main.py
@@ -2,40 +2,87 @@ from flask import Flask
 from flask import render_template, redirect, \
     request, session, url_for, escape
 
-from loginform import LoginForm
+from loginform import LoginForm, RegForm, BoxForm, ServiceForm, MarkForm, RefForm
 
 
 app = Flask(__name__)
 
 #!
-@app.route('/')
+
+@app.route('/Index.html')
+def main():
+    return redirect('/')
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
-@app.route('/box')
+@app.route('/box', methods=['GET', 'POST'])
 def box():
-    return render_template('Box.html')
+    form = BoxForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            print('logined:')
+            print(request.form['cod_name'])
+
+        else:
+            print('not valid')
+    return render_template('Box.html', form=form)
 
 
-@app.route('/service')
+@app.route('/service', methods=['GET', 'POST'])
 def service():
-    
-    return render_template('Service.html')
+    form = ServiceForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            print('logined:')
+            print(request.form['cod_owner'])
 
-@app.route('/ref')
+        else:
+            print('not valid')
+    return render_template('Service.html', form=form)
+
+@app.route('/ref', methods=['GET', 'POST'])
 def ref():
-    return render_template('Ref.html')
+    form = RefForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            print('logined:')
+            # print(request.form['cod_owner'])
+
+        else:
+            print('not valid')
+    return render_template('Ref.html', form=form)
 
 
-@app.route('/mark')
+@app.route('/mark', methods=['GET', 'POST'])
 def mark():
-    return render_template('Mark.html')
+    form = MarkForm(request.form)
+    #opt_list = list('345982')
+
+    if request.method == 'POST':
+        if form.validate():
+            print('logined:')
+            #print(request.form['cod_owner'])
+
+        else:
+            print('not valid')
+    return render_template('Mark.html', form=form)
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    return render_template("Registration.html")
+    form = RegForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            print('logined:')
+            print(request.form['name'], request.form['mid_name'], request.form['second_name'], request.form['adress'])
+
+        else:
+            print('not valid')
+    return render_template("Registration.html", form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
