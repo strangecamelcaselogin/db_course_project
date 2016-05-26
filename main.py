8778388fbc876e00a8eea1e3a49076fe32ac97dd
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2016-05-26 22:59:19 +0300

    Revert "totally new."
    This reverts commit 48832214f6fc5ea1b3ac5d98049fe57aeb5ee7aa.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from hashlib import sha256
+import os
 from functools import wraps
-
-import sqlite3 as lite
 
 from flask import Flask
 from flask import render_template, redirect, flash, \
-    request, session, abort, g, url_for
+    request, session, g, url_for
+
+import sqlite3 as lite
 
 from loginform import LoginForm, RegForm, BoxForm, ServiceForm, MarkForm, RefForm
 
-from settings import *
-
 
 app = Flask(__name__)
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+DATABASE = os.path.join(PROJECT_ROOT, 'base.db')
 
 
 def login_required(f):
@@ -30,34 +31,149 @@ def login_required(f):
 def admin_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if 'is_admin' in session:
+        if session['phone'] == '00000000':
             return f(*args, **kwargs)
         else:
-            abort(404)
+            flash('Вы не админ')
+            return redirect('/index')
 
     return wrapper
 
 
 ################################################
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', x=42)
+    return render_template('index.html')
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    flash(vars(request))
+    form = BoxForm(request.form)
+    return render_template('admin.html', form=form)
 
 
-@app.route('/services', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+
+    if request.method == 'POST':
+        if form.validate():
+
+            phone = form.phone.data
+            password = form.password.data
+
+            con = lite.connect(DATABASE)
+            with con:
+                cur = con.cursor()
+                cur.execute("SELECT * FROM Users WHERE Phone = :phone AND Password = :pass",
+                                {'phone': phone, 'pass': password})
+
+                data = cur.fetchall()
+
+                if len(data) != 0:
+                    session['logged_in'] = True
+                    session['phone'] = phone
+
+                    flash('Вошли как {}'.format(phone))
+                    return redirect('/index')
+
+            flash('Неверное имя пользователя или пароль.')
+
+        else:
+            flash('Что-то пошло не так...')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.clear()
+    flash('Вы вышли из системы.')
+
+    return redirect('/index')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegForm(request.form)
+
+    if request.method == 'POST':
+        if form.validate():
+            phone = form.phone.data
+            password = form.password.data
+
+            con = lite.connect(DATABASE)
+            with con:
+                cur = con.cursor()
+                cur.execute("SELECT * FROM Users WHERE Phone = :phone", {'phone':phone})
+
+                if len(cur.fetchall()) == 0:
+                    cur.execute("INSERT INTO Users VALUES (?,?)", (phone, password))
+                    flash('Вы зарегестрированы.')
+                    return redirect('/index')
+
+                else:
+                    flash('Такой пользователь уже существует.')
+
+    return render_template("registration.html", form=form)
+
+
+@app.route('/box', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def box():
+    form = BoxForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            flash('logined:')
+            #print(request.form['cod_name'])
+
+        else:
+            flash('not valid form: box')
+    return render_template('box.html', form=form)
+
+
+@app.route('/mark', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def mark():
+    form = MarkForm(request.form)
+
+    if request.method == 'POST':
+
+        try:
+            if request.form.get('mark_name') is not None:
+                print(request.form['mark_name'])
+            else:
+                print('nope mark name')
+
+            if request.form.get('mark_list') is not None:
+                print(request.form['mark_list'])
+            else:
+                print('nope mark list')
+        except Exception as e:
+            flash('error: ', e)
+
+    return render_template('mark.html', form=form)
+
+
+@app.route('/service', methods=['GET', 'POST'])
 @login_required
 def service():
     form = ServiceForm(request.form)
 
     if request.method == 'POST':
         if form.validate():
-            pass
+            print(request.form['cod_owner'])
 
         else:
             flash('not valid form: service')
-    return render_template('services.html', form=form)
+    return render_template('service.html', form=form)
 
 
 @app.route('/info', methods=['GET', 'POST'])
@@ -73,121 +189,7 @@ def ref():
     return render_template('info.html', form=form)
 
 
-@app.route('/admin_info', methods=['GET', 'POST'])
-@admin_required
-def admin_info():
-    return render_template('admin_info.html')
-
-
-@app.route('/boxes', methods=['GET', 'POST'])
-@admin_required
-def box():
-    form = BoxForm(request.form)
-    if request.method == 'POST':
-        if form.validate():
-            pass
-
-        else:
-            flash('not valid form: box')
-    return render_template('boxes.html', form=form)
-
-
-@app.route('/brands', methods=['GET', 'POST'])
-@admin_required
-def mark():
-    form = MarkForm(request.form)
-
-    if request.method == 'POST':
-        pass
-
-    return render_template('brands.html', form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm(request.form)
-
-    if request.method == 'POST':
-        if form.validate():
-
-            phone = form.phone.data
-            hashed_password = sha256(form.password.data.encode('utf-8')).hexdigest()
-
-            con = lite.connect(DATABASE)
-            with con:
-                cur = con.cursor()
-                cur.execute('''SELECT  * FROM Clients WHERE Phone = :phone AND Password = :password''',
-                            {'phone': phone, 'password': hashed_password})
-
-                data = cur.fetchall()
-
-                if len(data) == 1:
-                    session['logged_in'] = True
-                    session['phone'] = phone
-                    if phone == ADMIN:
-                        session['is_admin'] = True
-
-                    flash('Вы вошли как {}'.format(phone))
-                    return redirect('/index')
-
-            flash('Неверное имя пользователя или пароль.')
-
-        else:
-            flash('Что-то пошло не так...')
-
-    return render_template('login.html', form=form)
-
-
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
-    form = RegForm(request.form)
-
-    if request.method == 'POST':
-        if form.validate():
-            phone = form.phone.data
-            hashed_password = sha256(form.password.data.encode('utf-8')).hexdigest()
-
-            f_name = form.name.data
-            s_name = form.second_name.data
-            m_name = form.mid_name.data
-            address = form.address.data
-
-            con = lite.connect(DATABASE)
-            with con:
-                cur = con.cursor()
-
-                cur.execute("SELECT * FROM Clients WHERE Phone = :phone", {'phone': phone})
-
-                if len(cur.fetchall()) == 0:
-
-                    cur.execute('''INSERT INTO Clients (Second_Name, First_Name, Middle_Name, Address, Phone, Password)
-                                VALUES (:s_name, :f_name, :m_name, :address, :phone, :password)''',
-                                {'s_name': s_name,
-                                 'f_name': f_name,
-                                 'm_name': m_name,
-                                 'address': address,
-                                 'phone': phone,
-                                 'password': hashed_password})
-
-                    flash('Вы зарегестрированы.')
-                    return redirect('/index')
-
-                else:
-                    flash('Такой пользователь уже существует.')
-
-    return render_template("registration.html", form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    session.clear()
-    flash('Вы вышли из системы.')
-
-    return redirect('/index')
-
-
-################################################
+##########################################################
 
 
 if __name__ == '__main__':
