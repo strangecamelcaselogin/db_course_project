c00f09c50343a389dcd0e53151a64c6ae16ff5f3
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2016-06-12 23:10:47 +0300

    V2_1

---
+++ sql_core.py
@@ -0,0 +1,75 @@
+import sqlite3 as lite
+from hashlib import sha256
+
+from flask import session
+
+from settings import *
+
+
+def login(form):
+    phone = form.phone.data
+    hashed_password = sha256(form.password.data.encode('utf-8')).hexdigest()
+
+    con = lite.connect(DATABASE)
+    with con:
+        cur = con.cursor()
+        cur.execute('''SELECT  * FROM Clients WHERE Phone = :phone AND Password = :password''',
+                    {'phone': phone, 'password': hashed_password})
+
+        data = cur.fetchall()
+
+        if len(data) == 1:
+            session['logged_in'] = True
+            session['phone'] = phone
+            if phone == ADMIN:
+                session['is_admin'] = True
+
+            return True
+
+    return False
+
+
+def register(form):
+    phone = form.phone.data
+    hashed_password = sha256(form.password.data.encode('utf-8')).hexdigest()
+
+    f_name = form.name.data
+    s_name = form.second_name.data
+    m_name = form.mid_name.data
+    address = form.address.data
+
+    con = lite.connect(DATABASE)
+    with con:
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM Clients WHERE Phone = :phone", {'phone': phone})
+
+        if len(cur.fetchall()) == 0:
+            cur.execute('''INSERT INTO Clients (Second_Name, First_Name, Middle_Name, Address, Phone, Password)
+                            VALUES (:s_name, :f_name, :m_name, :address, :phone, :password)''',
+                        {'s_name': s_name,
+                         'f_name': f_name,
+                         'm_name': m_name,
+                         'address': address,
+                         'phone': phone,
+                         'password': hashed_password})
+
+            return True
+
+    return False
+
+
+def add_mark(mark_name):
+    con = lite.connect(DATABASE)
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Car_Brands WHERE Brand = :mark",
+                    {'mark': mark_name})
+
+        if len(cur.fetchall()) == 0:
+            cur.execute('''INSERT INTO Car_Brands (Brand) VALUES (:mark)''',
+                        {'mark': mark_name})
+            return True
+
+    return False
+
